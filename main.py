37f769b9193c9f71d04a464c89374a2ab2a5dc18
Datapacked <gaymario42069@gmail.com>
2022-08-03 15:01:17 -0400

    fixed minor things

--- main.py
+++ main.py
@@ -14,16 +14,17 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 """
 
-@bot.event
-async def on_message(message: str):
+@bot.slash_command(name="message")
+async def on_message(ctx, message: str):
 	f = open("message.txt", mode="a", encoding="utf-8")
-	f.write(message.content + "\n")
+	f.write(message + "\n")
 	f.close()
 
 @bot.slash_command(name="gen")
 async def gen(ctx):
-	await message.channel.send("Generating message ! please wait...")
-	await message.channel.send(textgen.generate(return_as_list=True)[0])
+	await ctx.defer()
+	await ctx.followup.send("Generating message ! please wait...")
+	await ctx.followup.send(textgen.generate(return_as_list=True)[0])
 
 
 client.run("ODQzMjE1MzI4MzU5MzUwMzIy.GWRaPM.cM9I6dsBkZSZMT5duZAlyzyi23uUHDz_HTEMxI")
